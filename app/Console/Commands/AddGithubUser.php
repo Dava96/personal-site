0050abd27cd89226bbd33bb360848f90480177a3
davidlomath <davidlomath@hotmail.co.uk>
2022-08-26 17:21:15 +0100

    Add command to add user and github post for testing

---
+++ app/Console/Commands/AddGithubUser.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Components\GithubSource;
+use App\Models\Category;
+use App\Models\GithubRepo;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class AddGithubUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'AddGithubUser {--post}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create an account that is used for testing';
+
+    public function __construct(GithubSource $githubSource)
+    {
+        $this->githubSource = $githubSource;
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        if (User::whereUsername('DavidLomathConnis')->exists()) {
+            $this->error('User already exists..');
+            return 0;
+        }
+
+        $user = new User([
+            'username' => 'DavidLomathConnis',
+            'github_username' => 'Dava96',
+            'name' => 'David Lomath',
+            'email' => 'david1@gmail.com',
+            'password' => 'password'
+        ]);
+
+        $user->save();
+
+        $this->info('User created: ' . $user);
+
+        if ($this->option('post')) {
+            $repoArray = $this->githubSource->getPostInfomationFromRepo('Dava96', 'StarterEdit');
+            $githubRepo = GithubRepo::make($repoArray);
+
+            $category = Category::firstOrCreate(['name' => $githubRepo->language, 'slug' => Str::slug($githubRepo->language)]);
+            $post = Post::create([
+                'title'       => $githubRepo->repo_name,
+                'slug'        => Str::slug($githubRepo->repo_name),
+                'excerpt'     => $githubRepo->description,
+                'body'        => $githubRepo->read_me,
+                'category_id' => $category->id,
+                'user_id'     => $user->id,
+            ]);
+
+            $this->info('Post added');
+            $post->repo()->save($githubRepo);
+            $githubRepo->save();
+            $post->save();
+        }
+        return 1;
+    }
+}
