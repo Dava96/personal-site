79d34b14adcc8073ef809293302f65c1cd6bab5e
davidlomath <davidlomath@hotmail.co.uk>
2022-10-21 17:09:59 +0100

    Add RegisterControllerTest.php

---
+++ tests/Feature/RegisterControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Components\GithubSource;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterControllerTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    public function testItRegistersANewUser()
+    {
+        $this->get(route('register.create'), [
+            'name' => $this->faker->name,
+            'username' => $this->faker->userName,
+            'github_username' => $this->faker->userName,
+            'email' => $this->faker->email,
+            'password' => $this->faker->password
+        ])
+        ->assertSuccessful();
+    }
+}
