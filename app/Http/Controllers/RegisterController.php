50fa3ba7ea2e768ff651cc8125c56cca82c0f7d6
davidlomath <davidlomath@hotmail.co.uk>
2022-10-24 09:07:26 +0100

    Remove unwanted imports on tests

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Components\GithubSource;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
--- tests/Feature/ReposApiTest.php
+++ tests/Feature/ReposApiTest.php
@@ -3,9 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\GithubRepo;
-use Database\Factories\GithubRepoFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ReposApiTest extends TestCase
